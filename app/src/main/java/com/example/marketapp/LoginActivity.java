78fdebfb4a2d022f2c3745686d01fbd073c4e86b
cuongGit01 <cuongndph11594@fpt.edu.vn>
2021-09-10 21:39:59 +0700

    Test 1

--- app/src/main/java/com/example/marketapp/LoginActivity.java
+++ app/src/main/java/com/example/marketapp/LoginActivity.java
@@ -65,6 +65,7 @@ public class LoginActivity extends AppCompatActivity {
                 finish();
             }
         });
+        //test
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
