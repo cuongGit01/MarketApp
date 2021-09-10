154505e52efb8f4e99f5624ec7d59ed56645ad7b
cuongGit01 <90460849+cuongGit01@users.noreply.github.com>
2021-09-10 21:41:35 +0700

    Update LoginActivity.java

--- app/src/main/java/com/example/marketapp/LoginActivity.java
+++ app/src/main/java/com/example/marketapp/LoginActivity.java
@@ -65,7 +65,7 @@ public class LoginActivity extends AppCompatActivity {
                 finish();
             }
         });
-        //test
+        //test123
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
